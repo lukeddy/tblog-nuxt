345cfc4149bf7bd730aeb9307e90096b196a5241
tzq668766 <tzq618766@gmail.com>
2019-01-21 15:11:42 +0800

    添加error页面，用于显示404等错误

---
+++ layouts/error.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container text-center error-wrapper">
+    <h3 v-if="error.statusCode === 404">404，页面没找到</h3>
+    <h3 v-else>出错了!</h3>
+    <nuxt-link to="/">回到首页</nuxt-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+  }
+</script>
+<style scoped>
+  .error-wrapper{
+    width: calc(100%);
+    height: calc(66vh);
+  }
+</style>
